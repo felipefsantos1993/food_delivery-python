e79cade6aafac96208d00f4f73771dd702d10213
Felipe Firmino Santos <felipefsantos.93@gmail.com>
2025-01-11 20:10:17 -0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -20,17 +20,25 @@ def stop_app():
     os.system('cls')
     print('The program has been finished...\n')
 
+def invalid_option():
+    input('Press any button to return...')
+    os.system('cls')
+    main()
+
 def choose_option():
-    option = int(input(f'Choose your option:\n'))
-    #print(f'You have chosen {option}')
-    if option == 1:
-        print(f'You have chosen: Register Restaurant')
-    elif option == 2:
-        print('You have chosen: List Restaurant')
-    elif option == 3:
-        print('You have chosen: Active Restaurant')
-    else:
-        stop_app()
+    try:
+        option = int(input(f'Choose your option:\n'))
+        #print(f'You have chosen {option}')
+        if option == 1:
+            print(f'You have chosen: Register Restaurant')
+        elif option == 2:
+            print('You have chosen: List Restaurant')
+        elif option == 3:
+            print('You have chosen: Active Restaurant')
+        elif option == 4:
+            stop_app()
+    except:
+        invalid_option()
 
 #def choose_option_match():
 #    option = int(input(f'Choose your option:\n'))
@@ -46,6 +54,7 @@ def choose_option():
 #            print(f'...')
 
 def main():
+    os.system('cls')
     show_program_name()
     show_program_options()
     choose_option()
