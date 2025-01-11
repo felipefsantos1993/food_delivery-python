ebc1acd52901af9aac631c88f9a405111a1208a4
Felipe Firmino Santos <felipefsantos.93@gmail.com>
2025-01-11 19:04:56 -0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -32,10 +32,25 @@ def choose_option():
     else:
         stop_app()
 
+def choose_option_match():
+    option = int(input(f'Choose your option:'))
+    #print(f'You have chosen {option}')
+    match option:
+        case 1:
+            print(f'You have chosen: Register Restaurant')
+        case 2:
+            print('You have chosen: List Restaurant')
+        case 3:
+            print('You have chosen: Active Restaurant')
+        case _:
+            print(f'...')
+
+
+
 def main():
     show_program_name()
     show_program_options()
-    choose_option()
+    choose_option_match()
 
 if __name__ == '__main__':
     main()
