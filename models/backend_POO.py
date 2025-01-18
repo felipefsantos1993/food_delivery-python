b15f8d5bd22d669ef1dccf23d92108f17d1c9875
Felipe Firmino Santos <felipefsantos.93@gmail.com>
2025-01-18 18:24:41 -0300

    Update backend_POO.py

--- models/backend_POO.py
+++ models/backend_POO.py
@@ -1,13 +1,37 @@
+class Musica:
+    nome = ''
+    artista = ''
+    duracao = int
 
-class restaurants:
-    Name = ''
-    Category = ''
-    Status = False
+musica1 = Musica()
+musica1.nome = 'Bohemian Rhapsody'
+musica1.artista = 'Queen'
+musica1.duracao = 355
 
-rodizio = restaurants()
-rodizio.Name = 'Didio'
-rodizio.Category = 'Gourmet'
+musica2 = Musica()
+musica2.nome = 'Imagine'
+musica2.artista = 'John Lennon'
+musica2.duracao = 183
 
-print(dir(rodizio))
-print(vars(rodizio))
-print(rodizio.Status)
+musica3 = Musica()
+musica3.nome = 'Shape of You'
+musica3.artista = 'Ed Sheeran'
+musica3.duracao = 234
+
+#"self" can be any word
+class Restaurants:
+    restaurant = []
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        self.status = False
+        Restaurants.restaurant.append(self)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
+    def restaurant_list():
+        for r in Restaurants.restaurant:
+            print(f'{r.name} | {r.category} | {r.status}')
+    
+super_burger = Restaurants('Super Burger', 'Fast Food')
+Restaurants.restaurant_list()
